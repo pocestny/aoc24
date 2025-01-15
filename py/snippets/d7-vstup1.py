2120ffef264133be28a77c8ec0b8b69671ff657c
mwinczer <winczer@fmph.uniba.sk>
2025-01-15 18:17:44 +0100

    Add files via upload

--- py/snippets/d7-vstup1.py
+++ py/snippets/d7-vstup1.py
@@ -4,3 +4,9 @@ for r in open('d7e.in'):
   # to iste ako predch. riadok, inak zapísané
   #n,ns=int(n),[int(x) for x in ns.split()] 
   # tu treba spracovať jedno zadanie úlohy
+
+
+#alternatívne čítanie vstupu využitím knižnice read
+import read
+for r in open('d7e.in'):
+  n,*ns=list(map(int,re.findall(r'(\d+):?\n?', r)))
