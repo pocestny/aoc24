cb4d8f9fbf030dcb850f403d9bf27d7dff7eeaeb
mwinczer <winczer@fmph.uniba.sk>
2025-01-22 18:38:53 +0100

    Add files via upload

--- py/snippets/d7-uprava2skratena.py
+++ py/snippets/d7-uprava2skratena.py
@@ -2,12 +2,10 @@ import time
 t,d=time.time(),0
 vys=0
 for n,ns in [it.split(':') for it in open('d7.in').read().split('\n')]:
-  n,ns=int(n),list(map(int,ns.split()))[::-1]
-  bol=[n]
-  for x in ns:
+  n,bol=int(n),[int(n)]
+  for x in map(int,ns.split()[::-1]):
     bol=[it//x for it in bol if it%x==0]+[it-x for it in bol if x<=it] \
          +[it//10**len(str(x)) for it in bol if str(it).endswith(str(x))] # len pre 2. časť
     d+=len(bol)
-  if 0 in bol:
-    vys+=n
+  vys+=n if 0 in bol else 0
 print(vys,'čas:',time.time()-t,'zápisov do pamäti:',d)
